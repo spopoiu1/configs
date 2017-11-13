1b89c362bb9b8186a16db85a5e8d3bcdac023f13
spopoiu1 <spopoiu@cloudbasesolutions.com>
2017-11-13 16:01:47 +0200

    Added retrive_go function

--- Install-brigade.sh
+++ Install-brigade.sh
@@ -1,22 +1,47 @@
 #!/bin/bash
 #this script should be run as a non root user
 # install docker:
+exec_with_retry2 () {
+    MAX_RETRIES=$1
+    INTERVAL=$2
 
-sudo apt-get update
-sudo apt-get install -y docker.io
+    COUNTER=0
+    while [ $COUNTER -lt $MAX_RETRIES ]; do
+        EXIT=0
+        eval '${@:3}' || EXIT=$?
+        if [ $EXIT -eq 0 ]; then
+            return 0
+        fi
+        let COUNTER=COUNTER+1
+
+        if [ -n "$INTERVAL" ]; then
+            sleep $INTERVAL
+        fi
+    done
+    return $EXIT
+}
+exec_with_retry () {
+    CMD=$1
+    MAX_RETRIES=${2-10}
+    INTERVAL=${3-0}
+
+    exec_with_retry2 $MAX_RETRIES $INTERVAL $CMD
+}
+exec_with_retry 'sudo apt-get update' 3 2
+exec_with_retry 'sudo apt-get install -y docker.io' 3 2
 
 # configure kubernetes apt repo:
 
-sudo apt-get install -y apt-transport-https
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+exec_with_retry 'sudo apt-get install -y apt-transport-https' 3 2
+exec_with_retry 'curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg' 3 2 | sudo apt-key add -
 sudo touch /etc/apt/sources.list.d/kubernetes.list
 sudo chmod 777 /etc/apt/sources.list.d/kubernetes.list
 sudo echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update
+exec_with_retry 'sudo apt-get update' 3 2
 
 # install kubernetes:
 
-sudo apt-get install -y kubelet kubeadm kubernetes-cni
+exec_with_retry 'sudo apt-get install -y kubelet kubeadm kubernetes-cni' 3 2
 
 # disable swap if exists (check: cat /proc/swaps)
 
@@ -46,7 +71,7 @@ kubectl taint nodes --all node-role.kubernetes.io/master-
 # install helm:
 # download desired version from https://github.com/kubernetes/helm/releases:
 
-wget https://storage.googleapis.com/kubernetes-helm/helm-v2.7.0-linux-amd64.tar.gz
+exec_with_retry 'wget https://storage.googleapis.com/kubernetes-helm/helm-v2.7.0-linux-amd64.tar.gz' 3 2
 tar -zxf helm-v2.7.0-linux-amd64.tar.gz
 
 # copy binaries into PATH:
@@ -59,11 +84,19 @@ helm init
 
 # create the binaries for brigade
 
-sudo apt-get update
-sudo apt-get -y upgrade
-wget https://storage.googleapis.com/golang/go1.9.2.linux-amd64.tar.gz
-tar -xvf go1.9.2.linux-amd64.tar.gz
-sudo mv go /usr/local
+exec_with_retry 'sudo apt-get update' 3 2
+exec_with_retry 'sudo apt-get -y upgrade' 3 2
+retrieve_go() {
+    local URL; local BASEFILE; local INSTALL_FOLDER; local TAR_FOLDER;
+    URL=$1
+    BASEFILE=$(basename $URL)
+ 
+    exec_with_retry "wget $URL" 3
+    TAR_FOLDER=$(sudo tar tzf $BASEFILE | head -1 | cut -f1 -d"/")
+    tar -xzf $BASEFILE
+    sudo  mv $TAR_FOLDER /usr/local
+}
+retrieve_go 'https://storage.googleapis.com/golang/go1.9.2.linux-amd64.tar.gz'
 export GOROOT=/usr/local/go
 echo "export GOROOT=/usr/local/go" | tee -a ~/.bashrc
 mkdir $HOME/work
@@ -75,13 +108,9 @@ mkdir -p $(go env GOPATH)/src/github.com/Azure
 git clone https://github.com/Azure/brigade $(go env GOPATH)/src/github.com/Azure/brigade
 pushd $(go env GOPATH)/src/github.com/Azure/brigade
 sudo apt install -y npm
-sudo apt install -y fakeroot
-last_error=$?
-if [[ $last_error == "1" ]];then
-fakeroot make bootstrap build
-fi
+sudo env PATH=$PATH GOROOT=$GOROOT GOPATH=$GOPATH make bootstrap build
 sudo cp $HOME/work/src/github.com/Azure/brigade/bin/* /usr/bin
-popd 
+popd
 # install brigade:
 # clone the repo:
 
@@ -96,15 +125,4 @@ kubectl create clusterrolebinding --user system:serviceaccount:default:brigade-b
 kubectl create clusterrolebinding --user system:serviceaccount:default:default kube-system-cluster-admin111 --clusterrole cluster-admin
 
 helm install --name brigade ./chart/brigade
-# create a test project:
-
-helm inspect values ./chart/brigade-project > myvalues.yaml
-# edit myvalues.yaml
-helm install --name my-project ./chart/brigade-project -f myvalues.yaml
-cat << EOF > brigade.js
-const { events } = require('brigadier')
-
-events.on("exec", (brigadeEvent, project) => {
-   console.log("Hello world!")
- })
-EOF
+popd
